8f75904faaf9b209b72c08d51d8bd7031070941f
Bhawani Shankar <bhaweshlohar2@gmail.com>
2021-10-07 14:36:25 +0530

    Add thing fixed

--- sprint-1/day-1/react_redux_calculator_part2/src/Redux/reducer.js
+++ sprint-1/day-1/react_redux_calculator_part2/src/Redux/reducer.js
@@ -5,32 +5,32 @@ export const reducerFn = (state, { type, payload}) => {
         case INC_COUNT:
             return {
                 ...state,
-                counter: state.counter + payload
-            }
+                counter: (+state.counter + payload).toFixed(2),
+            };
         case DEC_COUNT:
             return {
                 ...state,
-                counter: state.counter - payload
-            }
+                counter: (+state.counter - payload).toFixed(2),
+            };
         case ADD_COUNT:
             return {
                 ...state,
-                counter: state.counter + payload,
+                counter: (+state.counter + payload)
             };
         case SUB_COUNT:
             return {
                 ...state,
-                counter: state.counter - payload,
+                counter: (+state.counter - payload)
             };
         case MUL_COUNT:
             return {
                 ...state,
-                counter: state.counter * payload
-            }
+                counter: (+state.counter * payload)
+            };
         case DIV_COUNT:
             return {
                 ...state,
-                counter: (state.counter / payload).toFixed(2)
+                counter: (+state.counter / payload).toFixed(2)
             };
         default: return {...state}
     }
