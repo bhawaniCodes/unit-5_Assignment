a4ffa5f6005e61f3fbaf7dc5778b1844e1f27422
Bhawani Shankar <bhaweshlohar2@gmail.com>
2021-11-08 15:43:05 +0530

    made authController

--- Backend/Evaluation/BackendExam/src/controllers/auth.controller.js
+++ Backend/Evaluation/BackendExam/src/controllers/auth.controller.js
@@ -1,16 +1,44 @@
-require('dotenv').config();
-const User = require('../models/user.model');
+require("dotenv").config();
+const User = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+const { findOne } = require("../models/user.model");
 
 const createToken = (user) => {
     return jwt.sign({ user }, process.env.SECRET_KEY_JWT);
-}
+};
 
 const register = async (req, res) => {
-    user = await User.findOne({ email: req.body.email });
+    let user;
+    try {
+        user = await User.findOne({ email: req.body.email });
 
-    if (user) return res.status(400).send({message: "Email already exists"})
-    user = await
+        if (user)
+            return res.status(400).send({ message: "Email already exists" });
+        user = await User.create(req.body);
+        const token = createToken();
+        return res.status(200).send({ user, token });
+    } catch (err) {
+        return res.status(400).send({ message: "Something went wrong" });
+    }
+};
+
+const login = async (res, req)=>{
+    try {
+        let user = await User.findOne({ email: req.body.email }).lean().exec();
+        if (!user) {
+            return res.status(400).send({ message: "please check your email or password" });
+        }
+        let match = user.checkPassword(req.body.password);
+
+        if (!match) {
+            return res.status(400).send({ message: "please check your email or password" });
+        }
+
+        const token = createToken(user);
+        return res.status(200).send({ user, token });
+    } catch (err) {
+        return res.status(401).send({ message: "Something went wrong please try again" });
+    }
 }
 
-
+module.exports = {register, login}
--- Backend/Evaluation/BackendExam/src/models/user.model.js
+++ Backend/Evaluation/BackendExam/src/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
     {
@@ -13,6 +14,21 @@ const userSchema = mongoose.Schema(
         versionKey: false,
     }
 );
+
+userSchema.pre("save", function (next) {
+    if (!this.isModified("password")) {
+        return next()
+    }
+
+    const hash = bcryptjs.hashSync(this.password, 8);
+    this.password = hash;
+    return next();
+})
+
+userSchema.methods.checkPassword = function (password) { 
+    const match = bcryptjs.compareSync(password, this.password);
+    return match;
+}
 
 const User = mongoose.model('user', userSchema);
 module.exports = User;
